e5f8be2cb9e5732232c4febe819f13d85e6ab40e
eehantiming <weichenglau1991@gmail.com>
2021-04-14 14:54:01 +0800

    added exit command

--- opengripper.py
+++ opengripper.py
@@ -21,15 +21,22 @@ def move(position):
     robot.arm.set_ee_pose_pitch_roll(position, pitch=1.57, roll=0, plan=True)
 
 while True:
-    command = input("Command (open, close, move): ")
+    command = input("Command (open, close, move, exit): ")
     if command.lower() == 'open':
         open_gripper()
     elif command.lower() == 'close':
         close_gripper()
     elif command.lower() == 'move':
+        # move to absolute position
         position = input('Enter a list [x,y,z]: ')
+        # change string to python list
         position = json.loads(position)
         move(position)
+    elif command.lower() == 'exit':
+        robot.arm.set_ee_pose_pitch_roll([0.418, 0., 0.25], pitch=1.57, roll=0, plan=True)
+        break
     else:
         print('please enter a valid command.')
-    # TODO: add exit command
+
+
+# TODO: add GUI for convenience? 
